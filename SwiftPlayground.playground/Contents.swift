e8c260eb0c768d3e97ce2fe9d6f34e26b0ec2c6b
Andrew Firsenko <andrewfirsenko@gmail.com>
2026-03-09 13:28:32 +0300

    Remove extra files

--- SwiftPlayground.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-var unsafeSet: Set<String> = Set()
-
-DispatchQueue.concurrentPerform(iterations: 100) { _ in
-    unsafeSet.insert(Thread.current.description)
-}
-
-print(unsafeSet)
