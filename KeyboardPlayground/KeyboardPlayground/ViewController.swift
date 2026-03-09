4262d7f736d098cf16b7e6b465a5f3220d3dd473
Andrew Firsenko <andrewfirsenko@gmail.com>
2026-03-09 13:57:22 +0300

    Init KeyboardPlayground project

---
+++ KeyboardPlayground/KeyboardPlayground/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  KeyboardPlayground
+//
+//  Created by Andrew on 09.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+}
+
